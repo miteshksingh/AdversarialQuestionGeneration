7dd09ce63eee1498387ba03555a840c9c54345d7
Mitesh Singh <miksingh@wildgoose.compas.cs.stonybrook.edu>
2019-12-08 20:14:46 -0500

    adding BERT finetuning and predict scripts

---
+++ BERT/predict_squad.sh
@@ -0,0 +1,14 @@
+python run_squad.py \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_FINETUNED_DIR/model.ckpt-21899 \
+  --do_train=False \
+  --do_predict=True \
+  --predict_file=$SQUAD_DIR/train-v1.1.json \
+  --train_batch_size=8 \
+  --learning_rate=3e-5 \
+  --num_train_epochs=2.0 \
+  --max_seq_length=384 \
+  --doc_stride=128 \
+  --output_dir=./myoutputs/ \
+  --do_lower_case=False
---
+++ BERT/setenv.sh
@@ -0,0 +1,5 @@
+export SQUAD_DIR=/var/services/homes/miksingh/project/bert-qa/squad_data
+source /var/services/homes/miksingh/.bashrc
+export BERT_BASE_DIR=/var/services/homes/miksingh/project/bert-qa/downloaded_models/cased_L-12_H-768_A-12
+export GLUE_DATA=/var/services/homes/miksingh/project/bert-qa/downloaded_models/glue_data
+export BERT_FINETUNED_DIR=/var/services/homes/miksingh/project/bert-qa/finetuned_models/squad_base/final_model
